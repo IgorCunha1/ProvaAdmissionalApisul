7ef69b9e02d3beafd3fbc9eee6300513cebad9c1
igor Silva <igor.cunha@lumaensino.com.br>
2023-03-03 11:55:43 -0300

    Conclusao

--- ProvaAdmissionalApisul/Program.cs
+++ ProvaAdmissionalApisul/Program.cs
@@ -9,17 +9,39 @@ class Program
     {
         var elevadorService = new ElevadorService();
 
-        //var andarMenosUtilizado = elevadorService.andarMenosUtilizado();
-        //andarMenosUtilizado.ForEach(a => Console.Write(a+"-"));
+        var andarMenosUtilizado = elevadorService.andarMenosUtilizado();
+        Console.WriteLine("Andar Menos Utilizado: ");
+        Console.WriteLine(string.Join(",", andarMenosUtilizado));
+        
+        var elevadorMaisFrequentado = elevadorService.elevadorMaisFrequentado();
+        Console.WriteLine("Elevador Mais Frequentado: ");
+        Console.WriteLine(string.Join(",", elevadorMaisFrequentado));
 
-        //var elevadorMaisFrequentado = elevadorService.elevadorMaisFrequentado();
-        //elevadorMaisFrequentado.ForEach(a => Console.WriteLine(a));
-
-        //var elevadorMenosFrequentado = elevadorService.elevadorMenosFrequentado();
-        //elevadorMaisFrequentado.ForEach(a => Console.WriteLine(a));
-
+        var elevadorMenosFrequentado = elevadorService.elevadorMenosFrequentado();
+        Console.WriteLine("Elevador Menos Frequentado: ");
+        Console.WriteLine(string.Join(",", elevadorMenosFrequentado));
+        
         var percentualDeUsoElevadorA = elevadorService.percentualDeUsoElevadorA();
+        Console.WriteLine("Percentual De UsoElevador A: ");
         Console.WriteLine(percentualDeUsoElevadorA);
+        
+        var percentualDeUsoElevadorB = elevadorService.percentualDeUsoElevadorB();
+        Console.WriteLine("Percentual De UsoElevador B: ");
+        Console.WriteLine(percentualDeUsoElevadorB);
+        
+        var percentualDeUsoElevadorC = elevadorService.percentualDeUsoElevadorC();
+        Console.WriteLine("Percentual De Uso Elevador C: ");
+        Console.WriteLine(percentualDeUsoElevadorC);
+        
+        var percentualDeUsoElevadorD = elevadorService.percentualDeUsoElevadorD();
+        Console.WriteLine("Percentual De UsoElevador D: ");
+        Console.WriteLine(percentualDeUsoElevadorD);
+        
+        var percentualDeUsoElevadorE = elevadorService.percentualDeUsoElevadorE();
+        Console.WriteLine("Percentual De UsoElevador E: ");
+        Console.WriteLine(percentualDeUsoElevadorE);
+
+
 
     }
 }
